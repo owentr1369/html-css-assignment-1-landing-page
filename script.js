824d98d7ac5b33cffb91840e7b3cf2730272125d
owentr1369 <tamvaa1306@gmail.com>
2022-06-05 11:04:10 +0700

    add slider

---
+++ script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  slidesPerGroup: 3,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+});
